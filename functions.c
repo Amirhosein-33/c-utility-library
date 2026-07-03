60dade53c4a40df0693d277cce00837e2474411a
yazdan <mahdi.samimi.doost@gmail.com>
2026-07-04 01:15:19 +0330

    Add initial project structure

---
+++ functions.c
@@ -0,0 +1,13 @@
+#include "functions.h"
+
+int isPrime(int n) { return 0; }
+int factorial(int n) { return 0; }
+int reverseNumber(int n) { return 0; }
+int sumDigits(int n) { return 0; }
+int countDigits(int n) { return 0; }
+int gcd(int a, int b) { return 0; }
+int lcm(int a, int b) { return 0; }
+void reverseString(char str[]) { }
+int isOdd(int n) { return 0; }
+int isEven(int n) { return 0; }
+int power(int base, int exp) { return 0; }
---
+++ functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int isPrime(int n);
+int factorial(int n);
+int reverseNumber(int n);
+int sumDigits(int n);
+int countDigits(int n);
+int gcd(int a, int b);
+int lcm(int a, int b);
+void reverseString(char str[]);
+int isOdd(int n);
+int isEven(int n);
+int power(int base, int exp);
+
+#endif
