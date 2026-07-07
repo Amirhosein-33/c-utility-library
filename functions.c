3aa85364227c8f719f78ffbaa261265c15c7b174
Amirhosein-33 <amirhoseinalizadeh2007.1385@gmail.com>
2026-07-07 18:27:12 +0330

    Update functions.c

--- functions.c
+++ functions.c
@@ -2,7 +2,18 @@
 
 int isPrime(int n) { return 0; }
 int factorial(int n) { return 0; }
-int reverseNumber(int n) { return 0; }
+int reverseNumber(int n)
+{
+    int reverse = 0;
+
+    while (n != 0)
+    {
+        reverse = reverse * 10 + n % 10;
+        n /= 10;
+    }
+
+    return reverse;
+}
 int sumDigits(int n) { return 0; }
 
 int countDigits(int n) 
